59d56455db468f573acfb77f4f1a3255c7bf340c
Dan Shevlyuk <danshevlyuk@icloud.com>
2015-07-15 00:19:47 +0300

    InitialCommit

---
+++ Thermostat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thermostat
+//
+//  Created by Dan Shevlyuk on 15/07/15.
+//  Copyright (c) 2015 Team #19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
