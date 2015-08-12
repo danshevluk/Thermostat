e940211f7451a1f02a41fbfa7faf5de33e2934f2
Dan K <itsdanya@gmail.com>
2015-08-12 18:44:05 +0300

    Observe date changes

--- Thermostat/SettingsViewController.swift
+++ Thermostat/SettingsViewController.swift
@@ -10,29 +10,38 @@ import UIKit
 import DatePickerCell
 
 class SettingsViewController: UITableViewController {
-
+    
+    @IBOutlet weak var datePickerCell: DatePickerCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        datePickerCell.datePicker.addTarget(self, action: "datePicked:", forControlEvents: UIControlEvents.ValueChanged)
     }
-
+    
     // MARK: - Table view data source
-    override func tableView(tableView: UITableView,
-            didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        // Deselect automatically if the cell is a DatePickerCell.
-        if let cell = self.tableView(tableView, cellForRowAtIndexPath: indexPath)
-                as? DatePickerCell {
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Deselect automatically if the cell is a DatePickerCell
+        if let cell = self.tableView(tableView, cellForRowAtIndexPath: indexPath) as? DatePickerCell {
             cell.selectedInTableView(tableView)
             tableView.deselectRowAtIndexPath(indexPath, animated: true)
         }
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        // Get the correct height if the cell is a DatePickerCell.
-        if let cell = self.tableView(tableView, cellForRowAtIndexPath: indexPath)
-                as? DatePickerCell {
+        // Get the correct height if the cell is a DatePickerCell
+        if let cell = self.tableView(tableView, cellForRowAtIndexPath: indexPath) as? DatePickerCell {
             return cell.datePickerHeight()
         }
         
         return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
     }
+    
+    // MARK: - Observers
+    func datePicked(datePicker: UIDatePicker) {
+        let date = datePicker.date
+        println(date)
+        // Use it later, Luke
+    }
+    
 }
