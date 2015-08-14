1939c72e03f2ac9ae03fc97963d4eb22cd52af8f
Dan K. <itsdanya@gmail.com>
2015-08-15 01:30:48 +0300

    Install ActionSheetPicker, show date picker

--- Podfile
+++ Podfile
@@ -2,3 +2,4 @@ platform :ios, '8.0'
 use_frameworks!
 
 pod 'DatePickerCell'
+pod 'ActionSheetPicker-3.0'
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -52,8 +52,10 @@ code_sign_if_enabled() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework 'Pods/ActionSheetPicker_3_0.framework'
   install_framework 'Pods/DatePickerCell.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework 'Pods/ActionSheetPicker_3_0.framework'
   install_framework 'Pods/DatePickerCell.framework'
 fi
