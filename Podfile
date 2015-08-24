21b92fb71a4cc1f919741840d06f9335e3e32435
Dan Shevlyuk <danshevlyuk@icloud.com>
2015-08-25 00:17:13 +0300

    Remove old pods

--- Podfile
+++ Podfile
@@ -2,4 +2,3 @@ platform :ios, '8.0'
 use_frameworks!
 
 pod 'DatePickerCell'
-pod 'ActionSheetPicker-3.0'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// DatePickerCell
+#define COCOAPODS_POD_AVAILABLE_DatePickerCell
+#define COCOAPODS_VERSION_MAJOR_DatePickerCell 1
+#define COCOAPODS_VERSION_MINOR_DatePickerCell 0
+#define COCOAPODS_VERSION_PATCH_DatePickerCell 2
+
